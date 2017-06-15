af264f80022533eadf815f7fa89bf3309bb4f3f5
liuliang <784335750@qq.com>
2017-06-15 09:15:41 +0800

    增加版本号

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,26 @@ var webpack = require('gulp-webpack')
 // 引入文件名提取包
 var named = require('vinyl-named')
 
+// 版本号控制相关 包
+var rev = require('gulp-rev')
+var revCollector = require('gulp-rev-collector')
+var del = require('del')
+var gulpSequence = require('gulp-sequence')
+var minimist = require('minimist')
+var gulpif = require('gulp-if')
+var knownOptions = {
+  string: 'env',
+  default: { env: process.env.NODE_ENV || 'production'}
+}
+var options = minimist(process.argv.slice(2), knownOptions)
+// gulp load 包
+var $ = require('gulp-load-plugins')()
+
 // 引入gulp-sass包
 var sass = require('gulp-sass')
 
 //定义默认任务
-gulp.task('default',['copyhtml','webserver','watch','copyimage','packjs','packcss','copylibs'],function(){
+gulp.task('default',['pack','webserver','watch'],function(){
 	
 })
 //检测文件变化
@@ -26,14 +41,33 @@ gulp.task('watch', function() {
 	gulp.watch('./src/images/**/*', ['copyimage'])
 	gulp.watch('./src/libs/**/*', ['copylibs'])
 })
+//删除build下的文件
+gulp.task('clean',del.bind(null,['./build/**/*'],{
+	force:true
+}))
+
+//总打包  串行并行任务 [a,b,c]并行   外面串行  返回结果
+gulp.task('pack',function(callback){
+  if(options.env === 'production') {
+    gulpSequence('clean', ['packjs', 'packcss', 'copyhtml', 'copylibs', 'copyimage'], 'packhtml')(callback)
+  } else {
+    gulpSequence(['packjs', 'packcss', 'copyhtml', 'copylibs', 'copyimage'])(callback)
+  }
+})
 
 //把src的HTML文件拷贝到build
 gulp.task('copyhtml', function() {
-	gulp.src('./src/*.html')
-		.pipe(gulp.dest('./build/'))
+	return	gulp.src('./src/*.html')
+	.pipe(gulpif(options.env === 'production', gulp.dest('./build/')))
+    .pipe(gulpif(options.env !== 'production', gulp.dest('./dev/')))
 })
-
-
+//HTML增加版本号
+gulp.task('packhtml', function () {
+  return gulp.src(['./build/rev/**/*.json', './build/*.html'])
+    .pipe(revCollector())
+    .pipe($.minifyHtml())
+    .pipe(gulp.dest('./build'))
+})
 //启动一个webserver服务
 gulp.task('webserver', function() {
 	gulp.src('./build/')
@@ -76,7 +110,7 @@ gulp.task('webserver', function() {
 
 //打包js
 gulp.task('packjs',function(){
-	gulp.src([
+	return gulp.src([
 			'./src/script/app-login.js',
 			'./src/script/app.js',
 			'./src/script/app-invest.js',
@@ -102,12 +136,17 @@ gulp.task('packjs',function(){
 				]
 			}
 		}))
-		.pipe(gulp.dest('./build/script'))
+		.pipe(gulpif(options.env === 'production', $.uglify()))
+	    .pipe(gulpif(options.env === 'production', rev()))
+	    .pipe(gulpif(options.env === 'production', gulp.dest('./build/script')))
+	    .pipe(gulpif(options.env !== 'production', gulp.dest('./dev/script')))
+	    .pipe(gulpif(options.env === 'production', rev.manifest()))
+	    .pipe(gulpif(options.env === 'production', gulp.dest('./build/rev/script')))
 })
 
 //打包css
 gulp.task('packcss', function() {
-	gulp.src([
+return	gulp.src([
 			'./src/style/usage/app-login.scss',
 			'./src/style/usage/app-more.scss',
 			'./src/style/usage/app-account.scss',
@@ -117,15 +156,22 @@ gulp.task('packcss', function() {
 			'./src/style/usage/app-invest.scss'
 		])
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./build/style'))
+		.pipe(gulpif(options.env === 'production', $.minifyCss()))
+	    .pipe(gulpif(options.env === 'production', rev()))
+	    .pipe(gulpif(options.env === 'production', gulp.dest('./build/style')))
+	    .pipe(gulpif(options.env !== 'production', gulp.dest('./dev/style')))
+	    .pipe(gulpif(options.env === 'production', rev.manifest()))
+	    .pipe(gulpif(options.env === 'production', gulp.dest('./build/rev/style')))
 })
 // copy images
 gulp.task('copyimage', function() {
 	gulp.src('./src/images/**/*')
-		.pipe(gulp.dest('./build/images'))
+    .pipe(gulpif(options.env === 'production', gulp.dest('./build/images')))
+    .pipe(gulpif(options.env !== 'production', gulp.dest('./dev/images')))
 })
 // copy libs
 gulp.task('copylibs', function() {
 	gulp.src('./src/script/libs/*.*')
-		.pipe(gulp.dest('./build/libs'))
+    .pipe(gulpif(options.env === 'production', gulp.dest('./build/libs')))
+    .pipe(gulpif(options.env !== 'production', gulp.dest('./dev/libs')))
 })
